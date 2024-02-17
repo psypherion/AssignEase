3f3023796a93a2656ef76f7192aea9afa8b9e20e
Sayan Sarkar <72155617+ky13-troj@users.noreply.github.com>
2024-02-18 01:57:52 +0530

    Delete C_programs directory

--- C_programs/3_size-of.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("Size of int : %d\n", sizeof(int));
-    printf("Size of char : %d\n", sizeof(char));
-    printf("Size of double : %d\n", sizeof(double));
-    printf("Size of float : %d\n", sizeof(float));
-    return 0;
-}
--- C_programs/5_max-num.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int a, b;
-    printf("Enter Two Numbers : ");
-    scanf("%d %d", &a, &b);
-    printf("Maximum number is : %d", a > b ? a : b);
-    return 0;
-}
