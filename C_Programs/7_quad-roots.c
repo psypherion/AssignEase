1e8b5604df9f25e4fc569fabdea3c44b884c61f8
Sayan Sarkar <72155617+ky13-troj@users.noreply.github.com>
2024-02-20 04:10:50 +0530

    Delete C_Programs directory

--- C_Programs/7_quad-roots.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int a, b, c;
-    printf("Enter coefficients of quadratic equation : ");
-    scanf("%d %d %d", &a, &b, &c);
-    printf("Roots of the equation are : %0.2f %0.2f", (-b+sqrt(b*b-4*a*c))/(2*a), (-b-sqrt(b*b-4*a*c))/(2*a));
-}
